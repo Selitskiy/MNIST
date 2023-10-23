133e526e064f10c3caf8277a5e9e1e5ae899dad2
Selitskiy <77249557+Selitskiy@users.noreply.github.com>
2023-10-23 13:16:50 -0400

    Add files via upload

--- mnist.m
+++ mnist.m
@@ -4,6 +4,25 @@ clear all; close all; clc;
 
 addpath('~/ANNLib/');
 
+
+%load('mnist.mat');
+
+%XTrain = training.images;
+%YTrain = training.labels;
+
+%XTest = test.images;
+%YTest = test.labels;
+
+%n = training.height;
+%m = training.width;
+%l = training.count;
+%lts = test.count;
+
+%XTrainF = reshape(XTrain,[n*m,l]);
+%XTestF = reshape(XTest,[n*m,lts]);
+
+
+
 [XTrain,YTrain,anglesTrain] = digitTrain4DArrayData;
 [XTest,YTest,anglesTest] = digitTest4DArrayData;
 
@@ -12,17 +31,33 @@ addpath('~/ANNLib/');
 XTrainF = reshape(XTrain,[n*m,l,c]);
 XTestF = reshape(XTest,[n*m,l,c]);
 
-YTrainF = double(YTrain)';
-YTestF = double(YTest)';
 
-XTestF2 = XTestF;
+YTrainD = double(YTrain)';
+YTestD = double(YTest)';
 
+inj = 10;
+YTrainF = zeros(inj,l);
+YTestF = zeros(inj,l);
+
+for i = 1:inj
+    YTrainF(i, YTrainD==i) = 1;
+    YTestF(i, YTestD==i) = 1;
+end
+
+%%XTestF2 = XTestF;
+
+% real injection
+%XYTrainF = vertcat(XTrainF,YTrainD);
+%XYTestF = vertcat(XTestF,YTestD);
+
+% one-hot injection
 XYTrainF = vertcat(XTrainF,YTrainF);
 XYTestF = vertcat(XTestF,YTestF);
 
+
 %%
 x_off=0;
-x_in=n*m+1;
+x_in=n*m+inj;
 t_in=1;
 
 y_off=0;
@@ -30,15 +65,18 @@ y_off=0;
 y_out=n*m;
 t_out=t_in;
 
-ini_rate = 0.0001; 
-max_epoch = 100;
+ini_rate = 0.0002; 
+max_epoch = 500;
 
 regNet = Dp2BTransAEBaseNet2D(x_off, x_in, t_in, y_off, y_out, t_out, ini_rate, max_epoch, 1/n);
 
 %%
-            regNet.mb_size = 256;
+            regNet.mb_size = 512;
 
             regNet = Create(regNet);
+
+%%
+%load('digits_ae.mat', 'regNet');
 
 %%
 i=1;
@@ -52,6 +90,9 @@ i=1;
         % GPU off
         delete(gcp('nocreate'));
         gpuDevice([]); 
+
+%%
+%save('digits_ae.mat', 'regNet');
 
 %% activations
         % GPU on
@@ -94,8 +135,54 @@ XTestF2 = predictedScores';
 delete(gcp('nocreate'));
 gpuDevice([]);
 
+%% Generate
+XGenF = zeros([n*m, 10]);
+%XGenF = rand([n*m, 10]);
+%XYGenF = ones([n*m, 10]);
+
+YGenF = zeros([inj, 10]);
+
+% real inject
+%for i=1:10
+%    XYGenF(x_in, i) = i;
+%end
+
+% one hoot inject
+for i = 1:inj
+    YGenF(i, i) = 1;
+end
+
+XYGenF = vertcat(XGenF,YGenF);
+
+% GPU on
+gpuDevice(1);
+reset(gpuDevice(1));
+
+predictedScores = predict(regNet.trainedNet, XYGenF');
+XGenF2 = predictedScores';
+
+% GPU off
+delete(gcp('nocreate'));
+gpuDevice([]);
+
+%% Display Gen
+colormap(gray)
+colorbar
+
+for i=1:10
+    subplot(4,3,i);
+    If = XGenF2(:,i);
+    I2 = reshape(If, [n, m]);
+    
+    image(I2 .* 255);
+    title(string(i-1));
+end
+
 %%
 i = 1 + floor(rand()*l);
+
+colormap(gray)
+colorbar
 
 subplot(2,2,1);
 If = XTestF(:,i);
@@ -106,7 +193,7 @@ image(I2 .* 255);
 
 subplot(2,2,2);
 %Ifp = XTestF2(:,i);
-Ifp = actn(1:regNet.m_in-1,i);
+Ifp = actn(1:regNet.m_in-inj,i);
 I2p = reshape(Ifp, [n, m]);
 
 image(I2p .* 255);
